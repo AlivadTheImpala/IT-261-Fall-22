75b0e8df06e1e12cedf0a5f148541cb851584645
Brandon Davila <97662996+AlivadTheImpala@users.noreply.github.com>
2022-10-31 22:48:59 -0700

    added week 5 exercises and calculator

--- it261/index.php
+++ it261/index.php
@@ -17,7 +17,7 @@
     <ul>
       <li><a href="weeks/week3/switch.php">Switch</a></li>
       <li><a href="adder.php">Troubleshoot</a></li>
-      <li><a href="">Calculator</a></li>
+      <li><a href="weeks/week5/calculator4.php">Calculator</a></li>
       <li><a href="">Email</a></li>
       <li><a href="">Database</a></li>
       <li><a href="">Gallery</a></li>
@@ -73,6 +73,14 @@
         <li><a href="weeks/week4/form3.php">form3.php</a></li>
         <li><a href="weeks/week4/arithmetic-form.php">arithmetic-form.php</a></li>
         <li><a href="weeks/week4/celcius.php">celcius.php</a></li>
+      </ul>
+      <h3>Week 5</h3>
+      <ul>
+        <li><a href="weeks/week5/currency1.php">currency1.php</a></li>
+        <li><a href="weeks/week5/currency2.php">currency2.php</a></li>
+        <li><a href="weeks/week5/currency3.php">currency3.php</a></li>
+        <li><a href="weeks/week5/null.php">null.php</a></li>
+
       </ul>
     </aside>
   </div>
