4b28d6b7dd27e0b0656d534e15339f169eafcbdb
Brandon Davila <97662996+AlivadTheImpala@users.noreply.github.com>
2022-11-09 20:46:18 -0800

    updates index.php with week 6

--- it261/index.php
+++ it261/index.php
@@ -82,6 +82,14 @@
         <li><a href="weeks/week5/null.php">null.php</a></li>
 
       </ul>
+      <h3>Week 6</h3>
+      <ul>
+        <li><a href="weeks/week6/form.php">form.php</a></li>
+        <li><a href="weeks/week6/form2.php">form2.php</a></li>
+        <li><a href="weeks/week6/functions.php">functions.php</a></li>
+        <li><a href="weeks/week6/thx.php">thx.php</a></li>
+
+      </ul>
     </aside>
   </div>
   <!-- end wrapper -->
